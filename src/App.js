666f2e1b6ceaaaa13f9fdc9827f8750871b00b4f
symonmuchemi <simonkenya846@gmail.com>
2024-02-27 03:28:17 +0300

    used fetch api instead of axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig'
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [movie, setMovie] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/movies");
+      const movieData = await response.json()
+      console.log(movieData)
+      setMovie(movieData);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
